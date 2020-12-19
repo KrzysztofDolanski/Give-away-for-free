7f54056834e027a34291e679ef3e2525d193feab
KrzysztofDolanski <k.dolanski@wp.pl>
2020-12-19 12:08:05 +0100

    Initial commit

---
+++ src/main/java/com/example/gaff/GaffApplication.java
@@ -0,0 +1,13 @@
+package com.example.gaff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GaffApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gaff/GaffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gaff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
