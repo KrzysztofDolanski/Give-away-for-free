a42f93121f62b6849dc4dc31b04c9fd3a285b0a1
KrzysztofDolanski <k.dolanski@wp.pl>
2021-01-02 22:31:49 +0100

    Update User

--- src/main/java/com/example/gaff/image/UserFileRepository.java
+++ src/main/java/com/example/gaff/image/UserFileRepository.java
@@ -1,6 +1,18 @@
 package com.example.gaff.image;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 
 public interface UserFileRepository extends JpaRepository<UserFiles, Long> {
+    List<UserFiles> findUserFilesByUserId(Long userId);
+
+    @Modifying
+    @Query("delete from UserFiles as f where f.user.id = ?1 and f.modifiedFilename in(?2)")
+    void deleteUserFilesByUserIdAndFileName(Long id, List<String> removeImages);
+
 }
