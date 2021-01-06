e692a3bbe11bc033e3541b514dfbfbad481e1aa3
KrzysztofDolanski <k.dolanski@wp.pl>
2021-01-06 09:29:42 +0100

    User

--- src/main/java/com/example/gaff/exceptions/NoUsernameException.java
+++ src/main/java/com/example/gaff/exceptions/NoUsernameException.java
@@ -2,7 +2,8 @@ package com.example.gaff.exceptions;
 
 import java.util.EmptyStackException;
 
-public class NoUsernameException extends EmptyStackException {
+public class NoUsernameException extends RuntimeException {
     public NoUsernameException(String usenrame_must_be_write) {
+        super(usenrame_must_be_write);
     }
 }
