72a457f2250a5ba015253273aee33449e43de3a8
KrzysztofDolanski <k.dolanski@wp.pl>
2021-01-09 14:34:37 +0100

    Article save to DB

--- src/main/java/com/example/gaff/image/ArticleFileRepository.java
+++ src/main/java/com/example/gaff/image/ArticleFileRepository.java
@@ -3,8 +3,10 @@ package com.example.gaff.image;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+
 
 public interface ArticleFileRepository extends JpaRepository<ArticleFiles, Long> {
     List<ArticleFiles> findArticleFilesById(Long articleId);
