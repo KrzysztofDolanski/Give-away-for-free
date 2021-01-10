d0b2da2e4bd91100ebd13d39d36eb5f74e59be21
RitaBorowska <ritaboczulak@gmail.com>
2021-01-10 08:50:57 +0100

    repair booking classes

--- src/main/java/com/example/gaff/booking/BookingController.java
+++ src/main/java/com/example/gaff/booking/BookingController.java
@@ -8,12 +8,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
@@ -24,7 +22,7 @@ public class BookingController {
 
     final ArticleFetchService articleFetchService;
     final ApiUserService apiUserService;
-    final BookingServiceImpl bookingService;
+    final BookingService bookingService;
 
     @GetMapping("/bookings")
     public String showNewBookingPage(Model model) {
